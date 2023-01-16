939caa845db3d30cdd8d937a89aafe7012c3df01
Hyeon Kim <stuttgart.kim@gmail.com>
2023-01-16 18:21:36 +0100

    JS: perfect nav drop !

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
 'use strict';
 
-const nav0 = document.querySelector('.nav__drop.nav0');
-const nav1 = document.querySelector('.nav__drop.nav1');
-const nav2 = document.querySelector('.nav__drop.nav2');
+/* Nav Drop */
+document.addEventListener('click', navDropRemove);
 
-// Navbar : Our Services
-document.querySelector('.nav__linktext.nav0').addEventListener('click', () => {
-  nav0.classList.toggle('active');
-  nav1.classList.remove('active');
-  nav2.classList.remove('active');
-});
-
-// Navbar : Markant for..
-document.querySelector('.nav__linktext.nav1').addEventListener('click', () => {
-  nav1.classList.toggle('active');
-  nav0.classList.remove('active');
-  nav2.classList.remove('active');
-});
-
-// Navbar : About Us
-document.querySelector('.nav__linktext.nav2').addEventListener('click', () => {
-  nav2.classList.toggle('active');
-  nav0.classList.remove('active');
-  nav1.classList.remove('active');
-});
-
-// document.addEventListener('click', () => {
-//   document.querySelector('.nav__drop__box').classList.add('hidden');
-// });
+function navDropRemove(event) {
+  const index = event.target.dataset.index;
+  const nav = document.querySelectorAll('.nav__drop');
+  nav.forEach((navDrop) => {
+    if (index === navDrop.dataset.type) {
+      navDrop.classList.toggle('active');
+    } else {
+      navDrop.classList.remove('active');
+    }
+  });
+}
