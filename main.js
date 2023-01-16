ef5f65682363ac2f0b2630473e3e600cc8de0a47
Hyeon Kim <stuttgart.kim@gmail.com>
2023-01-16 23:21:25 +0100

    HTML, CSS, JS: counter

--- main.js
+++ main.js
@@ -14,3 +14,36 @@ function navDropRemove(event) {
     }
   });
 }
+
+/* Counter */
+
+// Selector
+const counters = document.querySelectorAll('.figures__item--figure');
+// Main function
+for (let n of counters) {
+  const updateCount = () => {
+    const target = +n.getAttribute('data-counter');
+    const count = +n.innerText;
+    const speed = 100; // animation speed
+    const inc = target / speed;
+    if (count < target) {
+      n.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      n.innerText = target;
+    }
+  };
+  updateCount();
+}
+
+/* Shoe 'arrow up' button when scrolling down */
+const home = document.querySelector('.header');
+const homeHeight = home.getBoundingClientRect().height;
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
