588a2b98608c0fed9de8bc163b45a23184bd3ea5
Hyeon Kim <stuttgart.kim@gmail.com>
2023-01-18 22:21:26 +0100

    JS: responsible 1st

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 'use strict';
 
 /* Nav Drop */
-document.addEventListener('click', navDropRemove);
+const nav = Array.from(document.querySelectorAll('.nav__linktext'));
 
-function navDropRemove(event) {
-  const index = event.target.dataset.index;
-  const nav = document.querySelectorAll('.nav__drop');
-  nav.forEach((navDrop) => {
-    if (index === navDrop.dataset.type) {
-      navDrop.classList.toggle('active');
-    } else {
-      navDrop.classList.remove('active');
-    }
+nav.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    const index = event.target.dataset.index;
+    const navDrop = document.querySelectorAll('.nav__drop');
+    navDrop.forEach((linkDrop) => {
+      if (index === linkDrop.dataset.type) {
+        linkDrop.classList.toggle('active');
+      } else {
+        linkDrop.classList.remove('active');
+      }
+    });
   });
-}
+});
+
+/* Nav Drop - Mobile */
+const toggleBtn = document.querySelector('.toggle__btn');
+const mobileDrop = document.querySelector('.mobile__drop');
+
+toggleBtn.addEventListener('click', () => {
+  mobileDrop.classList.toggle('invisible');
+});
+
+const mobileNavLink = Array.from(
+  document.querySelectorAll('.mobile__nav__link')
+);
+
+mobileNavLink.forEach((link) =>
+  link.addEventListener('click', (event) => {
+    const mType = event.target.dataset.mtype;
+    const drop = document.querySelectorAll('.nav__link2');
+    drop.forEach((linkDrop) => {
+      if (mType === linkDrop.dataset.drop) {
+        linkDrop.classList.toggle('active');
+        mobileDrop.classList.remove('invisible');
+      } else {
+        linkDrop.classList.remove('active');
+      }
+    });
+  })
+);
 
 /* Counter */
 const counterElements = document.querySelectorAll('.figures__item--figure');
